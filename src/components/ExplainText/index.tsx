e01bab03a15711bb20dbfd2085c4ccbe32e0a99f
Igor Couto <igor.fcouto@gmail.com>
2020-10-07 00:01:28 -0300

    Creating Explain Texts component

---
+++ src/components/ExplainText/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function ExplainText() {
+    return(
+        <div id="ExplainText">
+            <p>
+                A true random coin flip generator, powered by a quantum zero point energy field in vacuum.
+            </p>
+
+            <p>
+                Because computer entropy is for loosers.
+            </p>
+        </div>
+    );
+}
